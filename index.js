3d293fa9de56fbd94f3dab455c08163afadadea8
franamesticamolina-oss <franamesticamolina@gmail.com>
2026-07-16 22:45:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const video = document.getElementById('player');
+const url = 'https://192.168.1.1:557/live/stream.m3u8';
+if (Hls.isSupported()) {
+    const hls = new Hls();
 
+    hls.loadSource(url);
+    hls.attachMedia(video);
+
+    hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        video.play();
+    });
+
+} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = url;
+    video.play();
+}
